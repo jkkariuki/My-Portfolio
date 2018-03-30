41870f68ea48c58ac24153c1a4b2728ab8ae3670
jkkariuki <jkkariuki15@gmail.com>
2018-03-30 14:28:00 -0400

    no carousel

--- js/init.js
+++ js/init.js
@@ -3,7 +3,24 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-    
+$('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: false
+   });
+
+
+   // move next carousel
+   $('.moveNextCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('next');
+   });
+
+   // move prev carousel
+   $('.movePrevCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('prev');
+   });        
   }); // end of document ready
 })(jQuery); // end of jQuery name space
